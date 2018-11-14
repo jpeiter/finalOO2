1d04721c5adbf5da64840b4a5a43fa72a77a0cb3
Jean Felipe Peiter <jpeiter15@gmail.com>
2018-11-14 17:09:35 -0200

    .

--- src/main/java/br/edu/utfpr/pb/trabalhofinal/model/AbstractModel.java
+++
@@ -1,9 +0,0 @@
-package br.edu.utfpr.pb.trabalhofinal.model;
-
-import java.io.Serializable;
-
-public interface AbstractModel<ID extends Serializable>
-        extends Serializable {
-
-    ID getId();
-}
